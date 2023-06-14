83ba7dd6b42504a2883eefb78050f7365b76540d
Blake Rainwater <rainwaterblake@gmail.com>
2023-06-13 21:34:29 -0400

    Add support for large files! Also add some error handling and wifi connection retries

--- boot.py
+++ boot.py
@@ -19,20 +19,21 @@ except ImportError:
     raise
 
 def connectWifi():
-    try:
-        wifi.radio.connect(secrets["ssid"], secrets["password"])
-        needRetry = False
-    except Exception as ex:
-        print(ex)
+    for i in range(5):
+        try:
+            wifi.radio.connect(secrets["ssid"], secrets["password"])
+            break
+        except Exception as ex:
+            print(ex)
+            time.sleep(0.25)
 
 
-def getTree(session, headers):
+def getData(session, headers):
     response = session.request(
         "GET", secrets['github_code_url'],
         headers=headers)
-    print(response)
     try:
-        return response.json()['commit']['tree']
+        return response.json()
     except:
         print("Error, response not what expected!")
         print(response)
@@ -48,42 +49,84 @@ def getContents(session, headers, f):
     response = session.request(
         "GET", f['url'],
         headers=headers)
-    return response.json()['content']
+    return response
 
-try:
-    connectWifi()
-    pool = socketpool.SocketPool(wifi.radio)
-    session = Session(pool, ssl.create_default_context())
-    headers = None
-    if 'github_token' in secrets:
-        headers = {'Authorization': 'Bearer ' + secrets['github_token']}
-    tree = getTree(session, headers)
-    files = getFiles(session, headers, tree)
-    # Mount filesystem as writeable
-    remount('/', False)
-    paths = []
-    for f in files:
-        blob = getContents(session, headers, f)
-        data = a2b_base64(bytes(blob, 'utf-8'))
-        path = "/" + f['path'] + ".new"
-        paths.append(path)
-        with open(path, 'wb') as fobj:
-            fobj.write(data)
-        blob = None
-        gc.collect()
-    
-    print("Downloaded new files!")
-    for p in paths:
-        os.rename(p, p[:-4])
-        """with open(p[:-4], 'wb') as target:
-            with open(p, 'rb') as src:
-                target.write(src.read())"""
-    print("Updated files!")
 
-except Exception as ex:
-    print(ex)
-    raise ex
 
-finally:
-    # Always remount filesystem as read-only so we can write to it with the computer
-    remount('/', True)
+def isValidData(data):
+    for i in ['sha', 'commit']:
+        if i not in data:
+            print("Key: '" + i + "' not in response, unable to update!")
+            print(data)
+            return False
+    if 'tree' not in data['commit']:
+        print("Key: 'tree' not in response.json()['commit'], unable to update!")
+        print(data)
+        return False
+    if 'url' not in data['commit']['tree']:
+        print("Key: 'url' not in response.json()['commit']['tree'], unable to update!")
+        print(data)
+        return False
+    return True
+        
+
+def isUpToDate(data):
+    try:
+        with open('.ota-update', 'r') as otaf:
+            latestsha = data['sha']
+            mysha = otaf.readline().strip()
+            return mysha == latestsha
+    except:
+        print("Unable to find '.ota-update' file, assuming out-of-date code")
+        return False
+
+def main():
+    try:
+        connectWifi()
+        pool = socketpool.SocketPool(wifi.radio)
+        session = Session(pool, ssl.create_default_context())
+        headers = None
+        if 'github_token' in secrets:
+            headers = {'Authorization': 'Bearer ' + secrets['github_token']}
+        data = getData(session, headers)
+        if not isValidData(data):
+            print("Invalid data, not updating")
+            return
+        if isUpToDate(data):
+            print("Up to date, not updating code.")
+            return
+        tree = data['commit']['tree']
+        sha = data['sha']
+        del data
+        files = [f for f in getFiles(session, headers, tree) if f['type'] is not 'tree']
+        # Make github return the raw content instead of a base64 encoded json file with the content
+        headers['Accept'] = 'application/vnd.github.raw'
+        # Mount filesystem as writeable
+        remount('/', False)
+        paths = []
+        for f in files:
+            content_response = getContents(session, headers, f)
+            path = "/" + f['path'] + ".new"
+            paths.append(path)
+            with open(path, 'wb') as fobj:
+                for chunk in content_response.iter_content(4096):
+                    fobj.write(chunk)
+            del content_response
+            gc.collect()
+        
+        print("Downloaded new files!")
+        for p in paths:
+            os.rename(p, p[:-4])
+        with open('.ota-update', 'w') as otaf:
+            otaf.write(sha + '\n')
+        print("Updated files!")
+
+    except Exception as ex:
+        print(ex)
+        raise ex
+
+    finally:
+        # Always remount filesystem as read-only so we can write to it with the computer
+        remount('/', True)
+
+main()
